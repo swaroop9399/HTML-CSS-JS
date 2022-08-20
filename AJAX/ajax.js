9f4199217f15ecba847e9bbe71a4df630c9854c9
Swaroop Sanjay Patil <swaroop@apl.com>
2022-08-20 17:05:26 +0530

    HTML-CSS-JS

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -1,7 +1,6 @@
 console.log("AJAX tutorial in one video");
 
-
-document.getElementById("fetchBtn");
+let fetchBtn = document.getElementById("fetchBtn");
 fetchBtn.addEventListener('click', buttonClickHandler)
 
 function buttonClickHandler() {
@@ -10,7 +9,12 @@ function buttonClickHandler() {
     const xhr = new XMLHttpRequest();
 
     // Open the object
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true)
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true)
+
+    // USE THIS FOR POST REQUEST
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true)
+    xhr.getAllResponseHeaders('Content-type', 'application/x-www-form-urlencoded');
+
 
     // What to do on progress(optional)
     xhr.onprogress = function(){
@@ -28,7 +32,42 @@ function buttonClickHandler() {
     }
 
     // send the request
+    params = "name:test&salary:123&age=23"
+    xhr.send(params);
+
+    console.log("We are done!");
+}
+
+let popBtn = document.getElementById("popBtn");
+popBtn.addEventListener('click', popHandler)
+
+function popHandler(){
+    console.log('You have clicked the pop handler')
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET', 'https://dummy.restapiexample.com/api/v1/employees', true)
+
+    // What to do when response is ready
+    xhr.onload = function(){
+        if(this.status === 200){
+           let obj = JSON.parse(this.responseText)
+           console.log(obj)
+           let list = document.getElementById('list');
+           for (key in obj)
+           {
+                str == `<li>$(obj[key])</li>`;
+           }
+        }
+        else{
+            console.log("Some error occured")
+        }
+        
+    }
+
+    // send the request
     xhr.send();
 
-    console.log("We are done");
+    console.log("We are done fetchig employees!");
 }
